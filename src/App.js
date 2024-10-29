b9392b350122fa14aa45c832ea831fd2f8cd46af
Ayush <ayushmandal0122@gmail.com>
2024-10-29 12:11:33 +0530

    cors setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // src/App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/tasks')
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.error('Error fetching tasks:', error));
+  }, []);
 
   const addTask = (taskName) => {
     const newTask = { name: taskName, completed: false };
